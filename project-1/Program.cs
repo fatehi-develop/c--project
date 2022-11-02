a803321bbe3a51acc8be03d67bbd238f165c54b9
ali <fatehiali7466@gmail.com>
2022-11-02 22:25:26 +0330

    first commit 1401.8.11

---
+++ project-1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace c_
+{
+    class Test
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ project-2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace project_2
+{
+    class Booleans
+    {
+        public static void Main(string[] args)
+        {
+            bool content = true ;
+            bool nocontent = false ;
+            Console.WriteLine("test is {0}" , content );
+            Console.WriteLine("test is {0}" , nocontent );
+        }
+    }
+}
